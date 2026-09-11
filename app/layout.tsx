e7c2fade34be45362e8d821e474f5b5198a4846e
carolinasanchezgirona <carolinasanchezgirona@gmail.com>
2026-09-11 10:26:10 +0200

    Load moderated typography scale

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./mineuri-theme.css";
+import "./type-scale.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://carolinasanchezgirona.com"),
