31b6747b8089527fe24daee52bfd9726bfb6f6ea
carolinasanchezgirona <carolinasanchezgirona@gmail.com>
2026-09-17 08:13:15 +0200

    Keep private admin routes out of search crawling

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: ["/admin/", "/articulos/leer/"],
     },
     sitemap: "https://carolinasanchezgirona.com/sitemap.xml",
   };
