e65b3de48215e30a5018961d3e8757c3f3a160d4
carolinasanchezgirona <carolinasanchezgirona@gmail.com>
2026-09-18 22:02:48 +0200

    SEO: link dementia page from app/neuropsicologia/page.tsx

--- app/neuropsicologia/page.tsx
+++ app/neuropsicologia/page.tsx
@@ -144,6 +144,7 @@ export default function NeuropsychologyPage() {
               <a href="/evaluacion-neuropsicologica/">Evaluación neuropsicológica</a>
               <a href="/problemas-de-memoria/">Problemas de memoria y olvidos</a>
               <a href="/deterioro-cognitivo/">Deterioro cognitivo</a>
+              <a href="/demencias/">Demencias y Alzheimer</a>
               <a href="/psicologa-arenys-de-mar/">Consulta en Arenys de Mar</a>
               <a href="/psicologia/">Psicología General Sanitaria</a>
             </div>
