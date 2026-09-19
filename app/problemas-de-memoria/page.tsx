aaa8dc9e852f125d32f5c0e855896daeb2e03dc7
carolinasanchezgirona <carolinasanchezgirona@gmail.com>
2026-09-19 09:13:56 +0200

    SEO: añadir página de problemas de atención y concentración
    * SEO: add adult attention and concentration page
    
    * SEO: link adult attention page
    
    * SEO: link adult attention page
    
    * SEO: link adult attention page
    
    * SEO: link adult attention page
    
    * SEO: add adult attention page to sitemap

--- app/problemas-de-memoria/page.tsx
+++ app/problemas-de-memoria/page.tsx
@@ -160,6 +160,7 @@ export default function MemoryProblemsPage() {
               <a href="/deterioro-cognitivo-leve/">Deterioro cognitivo leve</a>
               <a href="/demencias/">Demencias y Alzheimer</a>
               <a href="/familiares-y-cuidadores-de-personas-con-demencia/">Familiares y cuidadores</a>
+              <a href="/problemas-de-atencion-y-concentracion-en-adultos/">Problemas de atención y concentración</a>
               <a href="/psicologa-arenys-de-mar/">Consulta en Arenys de Mar</a>
             </div>
           </div>
