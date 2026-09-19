ab34c4ec24eed6d62110011c43fbd5c330d64ec2
carolinasanchezgirona <carolinasanchezgirona@gmail.com>
2026-09-19 08:15:56 +0200

    SEO: link specific phobias page

--- app/psicologia/page.tsx
+++ app/psicologia/page.tsx
@@ -166,6 +166,7 @@ export default function PsychologyPage() {
               <a href="/pensamientos-intrusivos-y-miedo-a-perder-el-control/">Pensamientos intrusivos y miedo a perder el control</a>
               <a href="/toc-obsesiones-y-compulsiones/">TOC, obsesiones y compulsiones</a>
               <a href="/agorafobia-y-miedo-a-salir/">Agorafobia y miedo a salir</a>
+              <a href="/fobias-especificas/">Fobias específicas</a>
               <a href="/psicologa-arenys-de-mar/">Consulta en Arenys de Mar</a>
               <a href="/neuropsicologia/">Neuropsicología</a>
             </div>
