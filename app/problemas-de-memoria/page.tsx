d7c40c75f67937be032f20efdad80f7f8e34723b
carolinasanchezgirona <carolinasanchezgirona@gmail.com>
2026-09-19 09:25:46 +0200

    SEO: link epilepsy cognition page

--- app/problemas-de-memoria/page.tsx
+++ app/problemas-de-memoria/page.tsx
@@ -161,6 +161,7 @@ export default function MemoryProblemsPage() {
               <a href="/demencias/">Demencias y Alzheimer</a>
               <a href="/familiares-y-cuidadores-de-personas-con-demencia/">Familiares y cuidadores</a>
               <a href="/problemas-de-atencion-y-concentracion-en-adultos/">Problemas de atención y concentración</a>
+              <a href="/epilepsia-y-cambios-cognitivos/">Epilepsia y cambios cognitivos</a>
               <a href="/psicologa-arenys-de-mar/">Consulta en Arenys de Mar</a>
             </div>
           </div>
