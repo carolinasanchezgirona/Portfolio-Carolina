5a7afa1896c03f5b27b03d762aa0bae8ad39a8d2
carolinasanchezgirona <carolinasanchezgirona@gmail.com>
2026-09-19 09:22:17 +0200

    SEO: link multiple sclerosis cognition page

--- app/evaluacion-neuropsicologica/page.tsx
+++ app/evaluacion-neuropsicologica/page.tsx
@@ -144,6 +144,7 @@ export default function NeuropsychAssessmentPage() {
               <a href="/funciones-ejecutivas-y-planificacion-en-adultos/">Funciones ejecutivas y planificación</a>
               <a href="/conmocion-cerebral-y-tce-leve/">Conmoción cerebral y TCE leve</a>
               <a href="/parkinson-y-cambios-cognitivos/">Parkinson y cambios cognitivos</a>
+              <a href="/esclerosis-multiple-y-cambios-cognitivos/">Esclerosis múltiple y cambios cognitivos</a>
               <a href="/psicologa-arenys-de-mar/">Consulta en Arenys de Mar</a>
             </div>
           </div>
