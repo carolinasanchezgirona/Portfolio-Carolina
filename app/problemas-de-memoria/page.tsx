31787c8b58b7e01befe231b39af02ae46bf5fc64
carolinasanchezgirona <carolinasanchezgirona@gmail.com>
2026-09-19 08:43:13 +0200

    SEO: link mild cognitive impairment page

--- app/problemas-de-memoria/page.tsx
+++ app/problemas-de-memoria/page.tsx
@@ -157,6 +157,7 @@ export default function MemoryProblemsPage() {
               <a href="/neuropsicologia/">Neuropsicología</a>
               <a href="/evaluacion-neuropsicologica/">Evaluación neuropsicológica</a>
               <a href="/deterioro-cognitivo/">Deterioro cognitivo</a>
+              <a href="/deterioro-cognitivo-leve/">Deterioro cognitivo leve</a>
               <a href="/demencias/">Demencias y Alzheimer</a>
               <a href="/familiares-y-cuidadores-de-personas-con-demencia/">Familiares y cuidadores</a>
               <a href="/psicologa-arenys-de-mar/">Consulta en Arenys de Mar</a>
