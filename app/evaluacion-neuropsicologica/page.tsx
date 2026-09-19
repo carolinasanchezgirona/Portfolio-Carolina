fb2c60499c67b28b96021861ba1d5c75bb03b79d
carolinasanchezgirona <carolinasanchezgirona@gmail.com>
2026-09-19 09:11:38 +0200

    SEO: link adult attention page

--- app/evaluacion-neuropsicologica/page.tsx
+++ app/evaluacion-neuropsicologica/page.tsx
@@ -140,6 +140,7 @@ export default function NeuropsychAssessmentPage() {
               <a href="/deterioro-cognitivo-leve/">Deterioro cognitivo leve</a>
               <a href="/ictus-y-dano-cerebral-adquirido/">Ictus y daño cerebral adquirido</a>
               <a href="/afasia-y-problemas-de-lenguaje/">Afasia y problemas de lenguaje</a>
+              <a href="/problemas-de-atencion-y-concentracion-en-adultos/">Problemas de atención y concentración</a>
               <a href="/psicologa-arenys-de-mar/">Consulta en Arenys de Mar</a>
             </div>
           </div>
