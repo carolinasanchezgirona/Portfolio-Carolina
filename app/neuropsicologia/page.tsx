b72343bb95d1ee7942f58d0a29c2a0aa427caafa
carolinasanchezgirona <carolinasanchezgirona@gmail.com>
2026-09-19 08:36:48 +0200

    SEO: link dementia caregivers page

--- app/neuropsicologia/page.tsx
+++ app/neuropsicologia/page.tsx
@@ -146,6 +146,7 @@ export default function NeuropsychologyPage() {
               <a href="/deterioro-cognitivo/">Deterioro cognitivo</a>
               <a href="/demencias/">Demencias y Alzheimer</a>
               <a href="/estimulacion-cognitiva/">Estimulación cognitiva</a>
+              <a href="/familiares-y-cuidadores-de-personas-con-demencia/">Familiares y cuidadores</a>
               <a href="/psicologa-arenys-de-mar/">Consulta en Arenys de Mar</a>
               <a href="/psicologia/">Psicología General Sanitaria</a>
             </div>
