67f354b4d46955bd32e86fbcfb46b954e0d30f38
carolinasanchezgirona <carolinasanchezgirona@gmail.com>
2026-09-19 09:00:44 +0200

    SEO: link aphasia page

--- app/neuropsicologia/page.tsx
+++ app/neuropsicologia/page.tsx
@@ -149,6 +149,7 @@ export default function NeuropsychologyPage() {
               <a href="/familiares-y-cuidadores-de-personas-con-demencia/">Familiares y cuidadores</a>
               <a href="/deterioro-cognitivo-leve/">Deterioro cognitivo leve</a>
               <a href="/ictus-y-dano-cerebral-adquirido/">Ictus y daño cerebral adquirido</a>
+              <a href="/afasia-y-problemas-de-lenguaje/">Afasia y problemas de lenguaje</a>
               <a href="/psicologa-arenys-de-mar/">Consulta en Arenys de Mar</a>
               <a href="/psicologia/">Psicología General Sanitaria</a>
             </div>
